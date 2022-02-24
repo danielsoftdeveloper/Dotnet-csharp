16d7c91d3c2522f622d6913cb7f386eeb7354733
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2022-02-24 16:26:00 -0300

    created the c# dotnet framework.

---
+++ jogo-papel-tesoura-pedra/Program.cs
@@ -0,0 +1,15 @@
+﻿// Jogo Papel, Tesoura e Pedra
+
+using System;
+
+namespace jogoPapelTesouraPedra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jogo papel, tesoura e pedra");
+        }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Jogo Papel, Tesoura e Pedra 👨‍💻️👷️🏗️
+
+- Jogo para 2 jogadores 👬️
+- Desenvolvimento: Backend 📜️
+- Framework Dotnet 6.0
+- Linhagem: C# 
+- Platform: linux
+- Notebook: x86_64 
+- Distribuição :ubuntu
+- Version: 18.04
