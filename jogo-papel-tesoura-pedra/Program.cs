4796397852ef7b5ceb38efbd49df27e7be773ee1
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2022-03-10 16:16:02 -0300

    added try catch

--- jogo-papel-tesoura-pedra/Program.cs
+++ jogo-papel-tesoura-pedra/Program.cs
@@ -32,22 +32,22 @@ namespace jogoPapelTesouraPedra
             return result;
         }
 
-        static void showGameResult (int option1, int option2)
+        static void showGameResult(int option1, int option2)
         {
             if (option1 == option2)
-            {                
+            {
                 Console.WriteLine("As duas opções são iguais, então empatou");
             }
 
             else if (option1 == 1)
             {
                 if (option2 == 3)
-                {                    
+                {
                     Console.WriteLine("Jogador 1 ganhou!");
                     Console.WriteLine("A Pedra quebra a tesoura do Jogador 2");
                 }
                 else
-                {                    
+                {
                     Console.WriteLine("Jogador 2 ganhou");
                     Console.WriteLine("O papel embrulha a pedra");
                 }
@@ -60,7 +60,7 @@ namespace jogoPapelTesouraPedra
                     Console.WriteLine("O papel embrulha a pedra");
                 }
                 else
-                {                    
+                {
                     Console.WriteLine("Jogador 2 ganhou");
                     Console.WriteLine("A tesoura corta o papel");
                 }
@@ -74,7 +74,7 @@ namespace jogoPapelTesouraPedra
                     Console.WriteLine("A pedra quebra a tesoura");
                 }
                 else
-                {                    
+                {
                     Console.WriteLine("Jogador 1 Ganhou");
                     Console.WriteLine("A tesoura corta o papel");
                 }
@@ -84,41 +84,56 @@ namespace jogoPapelTesouraPedra
 
         static void Main(string[] args)
         {
-            int option1;
-            int option2;
-            string object1;
-            string object2;
+            int option1 = 0;
+            int option2 = 0;
+            string object1 = "Vazio";
+            string object2 = "Vazio"; ;
+            bool cancelaJogo = false;
 
             Console.Clear();
             Console.WriteLine("O jogo começou!");
 
-            do
+            try
             {
                 Console.WriteLine("Escolha uma opção entre (1) Pedra, (2) Papel ou (3)Tesoura.");
                 Console.Write("Jogador 1: ");
                 option1 = Convert.ToInt32(Console.ReadLine());
+                object1 = returnsObjectName(Convert.ToInt32(option1));                
                 Console.Clear();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro Jogador 1");
+                cancelaJogo = true;
+                Console.WriteLine(e.Message);                
+            }
 
-            } while (option1 < 1 || option1 > 3);
-
-            do
+            try
             {
                 Console.WriteLine("Escolha uma opção entre (1) Pedra, (2) Papel ou (3)Tesoura.");
                 Console.Write("Jogador 2: ");
                 option2 = Convert.ToInt32(Console.ReadLine());
+                object2 = returnsObjectName(Convert.ToInt32(option2));                
                 Console.Clear();
-
-            } while (option2 < 1 || option2 > 3);
-
-            object1 = returnsObjectName(Convert.ToInt32(option1));
-            object2 = returnsObjectName(Convert.ToInt32(option2));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro Jogador 2");
+                cancelaJogo = true;
+                Console.WriteLine(e.Message);
+            }
 
             Console.Clear();
             Console.WriteLine("Resultado: ");
             Console.WriteLine("\n");
-            showGameResult(option1, option2);
-
+            if (cancelaJogo)
             {
+                Console.WriteLine("Falha no Jogo");
+                Console.WriteLine("Jorgador 1: " + object1 + ", jogador 2: " + object2);
+            }
+            else
+            {
+                showGameResult(option1, option2);
                 Console.WriteLine("Jorgador 1: " + object1 + ", jogador 2: " + object2);
             }
         }
